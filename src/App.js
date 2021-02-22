e5b918358181d5267c60efa0923c9fbf35b816f6
springlordpants <springlordpants@gmail.com>
2021-02-22 13:15:28 -0800

    Added title, image and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from "react"
+import Chuck from "./media/chuck.jpg"
 
 export default function App() {
   return (
-    <div>
-      <h1>Chuck Norris API</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <h1 className="title">Chuck Norris API</h1>
+          <img src={Chuck} alt="chuck" className="chuck" />
+        </div>
+      </div>
     </div>
   )
 }
