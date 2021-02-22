887230224f2b8110b72e8a125ceca03eafb4f773
springlordpants <springlordpants@gmail.com>
2021-02-22 13:25:42 -0800

    Added search bar

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ export default function App() {
           <h1 className="title">Chuck Norris API</h1>
           <img src={Chuck} alt="chuck" />
         </div>
+
+        <div className="col-6 search-joke-col">
+          <div className="card">
+            <div className="card-header">Search for a word</div>
+            <div className="card-body">
+              <input class="search-option" type="text" />
+            </div>
+          </div>
+          <div>
+            <button className="btn btn-warning btn-lg">Generate Joke</button>
+          </div>
+        </div>
       </div>
     </div>
   )
